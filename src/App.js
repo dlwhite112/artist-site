a4f5761b24144d50a17e6356b38b4c119831be87
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2022-03-24 17:07:37 -0500

    update test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Artist page 
+          Artist page updated test
         </a>
       </header>
     </div>
